eb9615527b7e38298114683a1020c7125953137a
Tomek <tomo105@wp.pl>
2019-05-08 00:37:24 +0200

    linkedlist challenge in progress

--- src/LinkedList/Challenge1/Album.java
+++ src/LinkedList/Challenge1/Album.java
@@ -1,7 +1,5 @@
 package LinkedList.Challenge1;
 
-import sun.awt.image.ImageWatched;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -20,7 +18,6 @@ public class Album {
         if (findSong(title) == null) {
             this.songs.add(new Song(title, duration));
             return true;
-
         }
         return false;
     }
@@ -33,17 +30,17 @@ public class Album {
 
         return null;
     }
-
-    public boolean addToPlaylit(int trackNumber, LinkedList<Song> playList) {
+            //add using index in album
+    public boolean addToPlaylist(int trackNumber, LinkedList<Song> playList) {
         int index = trackNumber - 1;
-        if ((index > 0) && (index <= this.songs.size())) {
+        if ((index >= 0) && (index <= this.songs.size())) {
             playList.add(this.songs.get(index));
             return true;
         }
         System.out.println("failed to add song");
         return false;
     }
-
+    // add using title of the song
     public boolean addToPlaylist(String title, LinkedList<Song> playList) {
         Song checkedSong = findSong(title);
         if (checkedSong != null) {
@@ -55,3 +52,18 @@ public class Album {
     }
 }
 
+// Create a program that implements a playlist for songs
+// Create a Song class having Title and Duration for a song.
+// The program will have an Album class containing a list of songs.
+// The albums will be stored in an ArrayList
+// Songs from different albums can be added to the playlist and will appear in the list in the order
+// they are added.
+// Once the songs have been added to the playlist, create a menu of options to:-
+// Quit,Skip forward to the next song, skip backwards to a previous song.  Replay the current song.
+// List the songs in the playlist
+// A song must exist in an album before it can be added to the playlist (so you can only play songs that
+// you own).
+// Hint:  To replay a song, consider what happened when we went back and forth from a city before we
+// started tracking the direction we were going.
+// As an optional extra, provide an option to remove the current song from the playlist
+// (hint: listiterator.remove()
--- src/LinkedList/Challenge1/Main.java
+++ src/LinkedList/Challenge1/Main.java
@@ -1,7 +1,53 @@
 package LinkedList.Challenge1;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
+public class Main {
+
+    private static ArrayList<Album> albums = new ArrayList<Album>();
+
+    public static void main(String[] args) {
+        Album album = new Album("Nevermind", "Nirvana");
+        album.addSong("Rape me", 3.45);
+        album.addSong("Booze", 3.44);
+        album.addSong("Something", 5.38);
+        album.addSong("Lithium", 7.29);
+        album.addSong("Heart", 2.41);
+        album.addSong("Shaped", 1.42);
+        album.addSong("Box", 2.45);
+        albums.add(album);
+
+        Album album1 = new Album("Hybrid Theory", "Linkin Park");
+        album1.addSong("Runaway", 3.56);
+        album1.addSong("In the end", 3.45);
+        album1.addSong("Catalyst", 2.56);
+        album1.addSong("Blackout", 3.09);
+        album1.addSong("Graffiti", 2.43);
+        albums.add(album1);
+
+        LinkedList<Song> playlist = new LinkedList<Song>();
+     //   albums.get(0).addToPlaylist("Box", playlist);
+   //     albums.get(0).addToPlaylist("Booze", playlist);
+    //    albums.get(1).addToPlaylist(2, playlist);
+
+     //   albums.get(1).addToPlaylist(4, playlist);
+     //   albums.get(1).addToPlaylist(3, playlist);
+        albums.get(0).addToPlaylist(1, playlist);
+
+     play(playlist);
     }
+
+    private static void play(LinkedList<Song> playlist) {
+        ListIterator<Song> listIterator = playlist.listIterator();
+        if (playlist.size() == 0) {
+            System.out.println("No songs in your album ");
+            return;
+
+        } else {
+            System.out.println("You play " + listIterator.next().toString());
+        }
+    }
+
 }
